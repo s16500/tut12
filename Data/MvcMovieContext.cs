103302b2b9943ffc5fb62dfa79a830172dfc34f6
s16500 <s16500@pjwstk.edu.pl>
2020-06-05 14:07:45 +0200

    tut12

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tut12.Models;
+
+namespace Tut12.Data{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
